2ac83b90799bce305211aac35c9118e479b8bad9
merail <rail.me@google.com>
2022-02-15 18:29:56 +0300

    Greeting with expanding  by clicking

--- app/src/main/java/me/rail/jetpackcomposetest/MainActivity.kt
+++ app/src/main/java/me/rail/jetpackcomposetest/MainActivity.kt
@@ -3,23 +3,17 @@ package me.rail.jetpackcomposetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import me.rail.jetpackcomposetest.ui.theme.JetpackComposeTestTheme
@@ -28,112 +22,54 @@ class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            JetpackComposeTestTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colors.background
-//                ) {
-//                    Greeting("Android")
-//                }
-//            }
-
             JetpackComposeTestTheme {
-                Conversation(messages = SampleData.conversationSample)
+                MyApp()
             }
         }
     }
 }
 
-//@Composable
-//fun Greeting(name: String) {
-//    Text(text = "Hello $name!")
-//}
-
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    JetpackComposeTestTheme {
-//        Greeting("Android")
-//    }
-//}
-
-data class Message(val author: String, val body: String)
+@Composable
+fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
 
 @Composable
-fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource(R.drawable.ic_launcher_background),
-            contentDescription = "Contact profile picture",
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
-        )
+fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false) }
 
-        Spacer(modifier = Modifier.width(8.dp))
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
 
-        var isExpanded by remember { mutableStateOf(false) }
-
-        val surfaceColor: Color by animateColorAsState(
-            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
-        )
-
-        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
-            Text(
-                text = msg.author,
-                color = MaterialTheme.colors.secondaryVariant
-            )
-
-            Spacer(modifier = Modifier.height(4.dp))
-
-            Surface(
-                shape = MaterialTheme.shapes.medium, elevation = 1.dp,
-                color = surfaceColor,
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
                 modifier = Modifier
-                    .animateContentSize()
-                    .padding(1.dp)
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
             ) {
-                Text(
-                    text = msg.body,
-                    modifier = Modifier.padding(all = 4.dp),
-                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-                    style = MaterialTheme.typography.body2
-                )
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            OutlinedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
 }
 
-//    @Preview(name = "Light Mode")
-//    @Preview(
-//        uiMode = Configuration.UI_MODE_NIGHT_YES,
-//        showBackground = true,
-//        name = "Dark Mode"
-//    )
-//    @Composable
-//    fun PreviewMessageCard() {
-//        MdcTheme {
-//            MessageCard(
-//                msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
-//            )
-//        }
-//    }
-
+@Preview(showBackground = true, widthDp = 320)
 @Composable
-fun Conversation(messages: List<Message>) {
-    LazyColumn {
-        items(messages) { message ->
-            MessageCard(message)
-        }
-    }
-}
-
-@Preview
-@Composable
-fun PreviewConversation() {
+fun DefaultPreview() {
     JetpackComposeTestTheme {
-        Conversation(SampleData.conversationSample)
+        MyApp()
     }
 }
